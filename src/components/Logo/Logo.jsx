5ebf813f722875ef284940cc708a369d89a0a3c5
Filip Po <filip13.po@gmail.com>
2022-06-20 22:25:12 +0300

    Created Logo component

---
+++ src/components/Logo/Logo.jsx
@@ -0,0 +1,8 @@
+import logo from "../../assets/logo.svg";
+import "./Logo.scss";
+
+const Logo = () => {
+  return <img src={logo} className="logo" alt="Coook Service logo" />;
+};
+
+export default Logo;
